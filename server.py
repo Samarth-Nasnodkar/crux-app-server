5e30f9289d7644d1815752806577d3bc4bc68263
Samarth Nasnodkar <samarthnasnodkar@gmail.com>
2022-09-17 17:04:26 +0530

    First time adding coins to cloud firestore

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -6,13 +7,37 @@ from flask import Flask
 cred = credentials.Certificate("./creds.json")
 firebase_admin.initialize_app(cred)
 
-app = Flask(__name__)
+# app = Flask(__name__)
 client = firestore.client()
 
+coins = [
+    {
+        "name": "Doge",
+        "tag": "DOGE",
+        "icon": "assets/images/doge.png",
+        "price": 20.5,
+        "gain": 0.41,
+        "top": datetime.now()
+    },
+    {
+        "name": "Shiba Inu",
+        "tag": "SHIB",
+        "icon": "assets/images/doge.png",
+        "price": 0.04,
+        "gain": 0.3,
+        "top": datetime.now()
+    },
+    {
+        "name": "Avalanche",
+        "tag": "AVAX",
+        "icon": "assets/images/avax.png",
+        "price": 1052,
+        "gain": 1.3,
+        "top": datetime.now()
+    }
+]
 
-@app.route('/')
-def home():
-    return 'Hello!'
-
-
-app.run(host='0.0.0.0', port=8080)
+for coin in coins:
+    client.collection('coins').add(
+        coin
+    )
